9e352f25efe036e8d8f956d0e657fe6475c735d6
Sergey Lisov <sleirsgoevy@gmail.com>
2019-12-06 13:10:33 +0300

    xyu3

--- src/minion-ffi/example-c/main.c
+++ src/minion-ffi/example-c/main.c
@@ -27,25 +27,23 @@ int main() {
     struct Minion_Backend* backend;
     status = minion_backend_create(&backend);
     error_check(status);
-    struct Minion_DominionOptions dopts;
-    dopts.isolation_root = "/tmp/is";
-    dopts.process_limit = 1;
-    dopts.time_limit.seconds = 1;
-    dopts.time_limit.nanoseconds = 0;
-    struct Minion_SharedDirectoryAccess acc;
-    acc.kind = SHARED_DIRECTORY_ACCESS_KIND_READONLY;
-    acc.host_path = acc.sandbox_path = "/bin";
-    dopts.shared_directories = (struct Minion_SharedDirectoryAccess*) malloc(2 * sizeof(acc));
-    assert(dopts.shared_directories);
-    dopts.shared_directories[0] = acc;
-    dopts.shared_directories[1] = SHARED_DIRECTORY_ACCESS_FIN;
+    struct Minion_DominionOptions dopts = {
+        .time_limit = {1, 0},
+        .process_limit = 16,
+        .memory_limit = 0x40000000,
+        .isolation_root = "/tmp/is",
+        .shared_directories = (struct Minion_SharedDirectoryAccess[2]){
+            {SHARED_DIRECTORY_ACCESS_KIND_READONLY, "/bin", "/bin"},
+            SHARED_DIRECTORY_ACCESS_FIN,
+        },
+    };
     struct Minion_Dominion* dominion;
     status = minion_dominion_create(backend, dopts, &dominion);
     error_check(status);
     struct Minion_ChildProcessOptions cpopts = {
-        .image_path = "/bin/ls",
-        .argv = (char*[2]){"ls", NULL},
-        .envp = &ENV_ITEM_FIN,
+        .image_path = "/bin/busybox",
+        .argv = (char*[3]){"ls", "/proc", NULL},
+        .envp = (struct Minion_EnvItem[1]){ENV_ITEM_FIN},
         .stdio = {0, 1, 2},
         .dominion = dominion,
         .workdir = "/",
@@ -53,5 +51,18 @@ int main() {
     struct Minion_ChildProcess* cp;
     status = minion_cp_spawn(backend, cpopts, &cp);
     error_check(status);
-    for(int i = 0; i < (1 << 31) - 1; i++);
+    Minion_WaitOutcome outcome;
+    status = minion_cp_wait(cp, &(struct Minion_TimeSpec){5, 0}, &outcome);
+    error_check(status);
+    if(status == WAIT_OUTCOME_TIMEOUT)
+    {
+        status = minion_cp_kill(cp);
+        error_check(status);
+    }
+    status = minion_cp_free(cp);
+    error_check(status);
+    status = minion_dominion_free(dominion);
+    error_check(status);
+    status = minion_backend_free(backend);
+    error_check(status);
 }
